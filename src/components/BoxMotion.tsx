d3d2c04c255413ec098a74c32100a1a86450ecb1
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-20 21:57:31 +0600

    stagger animation

--- src/components/BoxMotion.tsx
+++ src/components/BoxMotion.tsx
@@ -1,13 +1,26 @@
 import { motion } from 'framer-motion';
-
+const parent = {
+   // initial:{rotate:0},
+   // animate:{rotate:360},
+   hidden: {opacity:0, scale:0.9},
+   visible:{opacity:1,scale:1.1}
+}
 const BoxMotion = () => {
    return (
       <motion.div
-       className='size-64 rounded-lg bg-indigo-500 '
-       initial={{rotate:0}}
-       animate={{rotate:360}}
-       transition={{ type:'spring', duration:3}}
-       ></motion.div>
+       className='flex flex-wrap justify-center items-center gap-5 p-5 size-64 rounded-lg bg-indigo-500 '
+       variants={parent}
+      //  initial="initial"
+      //  animate="animate"
+      initial="hidden"
+      animate="visible"
+      transition={{ ease:"easeInOut" , duration:1.5}}
+       >
+         <motion.div className='size-20 rounded-sm bg-cyan-400'></motion.div>
+         <motion.div className='size-20 rounded-sm bg-cyan-400'></motion.div>
+         <motion.div className='size-20 rounded-sm bg-cyan-400'></motion.div>
+         <motion.div className='size-20 rounded-sm bg-cyan-400'></motion.div>
+       </motion.div>
    );
 };
 
