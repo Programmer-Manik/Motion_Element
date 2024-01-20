bfb193f528abc0f2d46710ee536a5d3211defb09
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-21 00:50:33 +0600

    Gesture - Drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 
 import './App.css'
 //import BoxMotion from './components/BoxMotion'
-import Lesson3 from './components/Lesson3'
+// import Lesson3 from './components/Lesson3'
+import Lesson4 from './components/Lesson4'
 function App() {
 
   return (
     <div className='flex h-screen w-full justify-center items-center'>
        {/* <BoxMotion /> */}
-       <Lesson3/>
+       {/* <Lesson3/> */}
+       <Lesson4/>
     </div>
   )
 }
---
+++ src/components/Lesson4.tsx
@@ -0,0 +1,45 @@
+import { motion } from "framer-motion";
+import { useRef } from "react";
+const parent = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { 
+               opacity: 0.5, 
+               scale: 0.7 ,
+               transition:{
+                  ease: "easeInOut",
+                  duration:2,
+                }
+            },
+   hover:{
+     opacity:1,
+   },
+
+};
+
+const Lesson4 = () => {
+   const parentRef = useRef(null);
+   return (
+      <div
+      ref={parentRef}
+      className="size-[500px] flex justify-center items-center border border-red-100">
+      <motion.div
+        className="flex flex-wrap justify-center items-center gap-5 p-5 size-64 rounded-lg bg-indigo-500"
+        variants={parent}
+        initial="hidden"
+        animate="visible"
+        whileHover="hover"
+       // whileTap="tap"
+        //onHoverStart={() => console.log('onHover')}
+       // onHoverEnd={() => console.log('endHover')}
+        drag
+      //   dragSnapToOrigin
+        dragElastic={1}
+      //   dragConstraints={{left:-200, right:200, top:-200, bottom:200}}
+        dragConstraints={parentRef}
+      >
+      </motion.div>
+    </div>
+   );
+};
+
+export default Lesson4;
