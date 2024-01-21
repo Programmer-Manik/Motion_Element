873d3bd80aa3b0d8886139e2be6a44e1e5e2f8e6
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-22 00:26:05 +0600

    add hook framer motion

--- src/components/Lesson6.tsx
+++ src/components/Lesson6.tsx
@@ -1,65 +1,39 @@
-import { motion } from "framer-motion";
-import { useRef } from "react";
-const parent = {
-  hidden: { 
-    scale: 0.9,
-     x:0,
-     y:0,
-   opacity:0
-   },
-  visible: {
-    x:[0,300,-300,0],
-    y:[0,300,-300,0],
-    scale: 0.7,
-    opacity:1,
-    rotate:[0,300,-300,0],
-    transition: {
-      ease: "linear",
-      repeat:Infinity,
-      duration: 5,
-      opacity:{
-         duration:0.5,
-      },
-      rotate:{
-         duration:1,
-         delay:0.5,
-         repeat:Infinity
-      }
-    },
-  },
-  hover: {
-    opacity: 1,
-  },
-};
+import {motion, useAnimationControls} from 'framer-motion'
+import { useEffect } from 'react';
 
 const Lesson6 = () => {
-  const parentRef = useRef(null);
-  return (
-    <div
-      ref={parentRef}
-      className="size-[500px] flex justify-center items-center border border-red-100"
-    >
-      <motion.div
-        className="flex flex-wrap justify-center items-center gap-5 p-5 size-64 rounded-lg bg-indigo-500"
-        variants={parent}
-        initial="hidden"
-        animate="visible"
-        whileHover="hover"
-        // whileTap="tap"
-        //onHoverStart={() => console.log('onHover')}
-        // onHoverEnd={() => console.log('endHover')}
-        drag
-        //   dragSnapToOrigin
-        dragElastic={1}
-        //   dragConstraints={{left:-200, right:200, top:-200, bottom:200}}
-        dragConstraints={parentRef}
-        whileDrag={{
-          scale: 1.1,
-          boxShadow: "0px 10px 10px #000",
-        }}
-      ></motion.div>
-    </div>
-  );
+   const controls = useAnimationControls()
+   // const [toggle, setToggle] = useState(false)
+   useEffect(()=> {
+         controls.start((i)=>({x:-200,transition:{duration:5,delay:i*1}}))
+   },[controls])
+   return (
+      <div
+      className="size-[500px] flex flex-col items-center justify-center  border border-red-100"
+      >
+         <motion.div
+         className="flex flex-wrap justify-center items-center gap-5 p-5 size-64 rounded-lg bg-indigo-500"
+         animate={controls}
+         custom={1}
+         >
+            
+         </motion.div>
+         <motion.div
+         className="flex flex-wrap justify-center items-center gap-5 p-5 size-64 rounded-lg bg-indigo-500"
+         animate={controls}
+         custom={2}
+         >
+            
+         </motion.div>
+         <motion.div
+         className="flex flex-wrap justify-center items-center gap-5 p-5 size-64 rounded-lg bg-indigo-500"
+         animate={controls}
+         custom={3}
+         >
+            
+         </motion.div>
+      </div>
+   );
 };
 
 export default Lesson6;
