86bf2812afcef9cf031f973486c10c1064060aca
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-23 01:06:34 +0600

    AnimatePresence motion fermer

--- src/components/Lesson13.tsx
+++ src/components/Lesson13.tsx
@@ -1,21 +1,40 @@
-import { motion, } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 const Lesson13 = () => {
-   const box = {
-      hidden: {opacity:0.5, scale:0.5},
-      visible:{opacity:1, scale:1,
-         transition:{
-            duration:1.5
-         },
+  const [visible, setVisible] = useState(true);
+  const box = {
+    hidden: { opacity: 0.5, scale: 0.5 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 1.5,
       },
-   };
+    },
+    exit: {
+      opacity: 0,
+      y: 1000,
+      transition: {
+        duration:0.5,
+      },
+    },
+  };
   return (
     <div className="size-[500px] flex flex-col items-center justify-center  border border-red-100">
-      <motion.div 
-      className="flex flex-wrap justify-center items-center gap-5 p-5 size-64 rounded-lg bg-indigo-500"
-      variants={box}
-      initial='hidden'
-      animate="visible"
-      ></motion.div>
+      <AnimatePresence>
+        <motion.button layout onClick={() => setVisible(!visible)}>
+          toggle
+        </motion.button>
+        {visible && (
+          <motion.div
+            className="flex flex-wrap justify-center items-center gap-5 p-5 size-64 rounded-lg bg-indigo-500"
+            variants={box}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
